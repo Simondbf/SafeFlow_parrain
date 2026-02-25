3e8225a9f72a28847d8262797e2a88a14de593e9
Simondbf <64691542+Simondbf@users.noreply.github.com>
2026-02-25 15:12:22 +0100

    Create filleul_model.dart

---
+++ lib/models/filleul_model.dart
@@ -0,0 +1,13 @@
+class Filleul {
+  final String id; // ID unique pour identification claire
+  final String nom;
+  final String dateInscription;
+  final String status;
+
+  Filleul({
+    required this.id,
+    required this.nom,
+    required this.dateInscription,
+    required this.status,
+  });
+}
