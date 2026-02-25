297608b713ae97ef1d88cbe7e901d71467ffb1e5
Simondbf <64691542+Simondbf@users.noreply.github.com>
2026-02-25 15:14:30 +0100

    Create parametres_screen.dart

---
+++ lib/screens/dashboard/parametres_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ParametresScreen extends StatelessWidget {
+  const ParametresScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Paramètres')),
+      body: const Center(child: Text('Paramètres (à implémenter)')),
+    );
+  }
+}
+
